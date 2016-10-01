d729c830fde4e907322a970ca4c2890069898f4c
Simona Francini <simoncinaholly@gmail.com>
2016-10-01 10:57:34 +0200

    Test improved

--- writersutility-plugin/src/test/java/it/holls/writersutility_plugin/ServiceTest.java
+++ writersutility-plugin/src/test/java/it/holls/writersutility_plugin/ServiceTest.java
@@ -10,16 +10,38 @@ import it.holls.writersutility_plugin.service.GUIService;
 public class ServiceTest {
 	
 	@Test
-	public void checkEditability(){
+	public void checkEditabilityForEditorPane(){
 		GUI gui = new GUI();
 		
 		GUIService guiService = GUIService.getInstance();
 		guiService.setGUI(gui);
+		
+		guiService.setOriginalText("Prova prova");
+		
+		assertEquals(guiService.getOriginalText(), "<html><head></head><body><div>Prova prova</div></body></html>");
 		
 		guiService.setGUIText("<html><head></head><body><p>Prova prova</p></body></html>");
 		
 		assertEquals(guiService.getGUIText().replaceAll("\n", "").replaceAll(">(\\s)*", ">").replaceAll("(\\s)*<", "<"), "<html><head></head><body><p>Prova prova</p></body></html>");
 		assertEquals(gui.getText().replaceAll("\n", "").replaceAll(">(\\s)*", ">").replaceAll("(\\s)*<", "<"), "<html><head></head><body><p>Prova prova</p></body></html>");
 		
+	}
+	
+	@Test
+	public void checkEditabilityForHTMLCode(){
+		GUI gui = new GUI();
+		
+		GUIService guiService = GUIService.getInstance();
+		guiService.setGUI(gui);
+		
+		guiService.setOriginalText("<html><head></head><body><p>Prova prova</p></body></html>");
+		
+		assertEquals(guiService.getOriginalText(), "<html><head></head><body><p>Prova prova</p></body></html>");
+		
+		guiService.setGUIText("<html><head></head><body><p>Prova prova 2</p></body></html>");
+		
+		assertEquals(guiService.getGUIText().replaceAll("\n", "").replaceAll(">(\\s)*", ">").replaceAll("(\\s)*<", "<"), "<html><head></head><body><p>Prova prova 2</p></body></html>");
+		assertEquals(gui.getText().replaceAll("\n", "").replaceAll(">(\\s)*", ">").replaceAll("(\\s)*<", "<"), "<html><head></head><body><p>Prova prova 2</p></body></html>");
+		assertEquals(guiService.getOriginalText(), "<html><head></head><body><p>Prova prova</p></body></html>");
 	}
 }
